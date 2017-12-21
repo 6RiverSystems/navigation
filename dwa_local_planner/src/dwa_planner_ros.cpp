2742e4aaf53cc48eb37b80a10cd750de68f8d150
Dan Winkler <dan@6river.com>
2017-12-21 15:33:36 -0500

    fix: Removed dwa info log.

--- dwa_local_planner/src/dwa_planner_ros.cpp
+++ dwa_local_planner/src/dwa_planner_ros.cpp
@@ -349,7 +349,7 @@ namespace dwa_local_planner {
       if (isOk) {
         publishGlobalPlan(transformed_plan);
       } else {
-        ROS_WARN_NAMED("dwa_local_planner", "DWA planner failed to produce path.");
+        ROS_DEBUG_NAMED("dwa_local_planner", "DWA planner failed to produce path.");
         std::vector<geometry_msgs::PoseStamped> empty_plan;
         publishGlobalPlan(empty_plan);
       }
